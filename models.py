7ad65855e9e0515076111c07659e4a012b0b8b6e
LilMike707 <michaelmarracq@yahoo.com>
2023-02-16 20:23:06 -0800

    part 3 start

--- models.py
+++ models.py
@@ -33,3 +33,19 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+class Tag(db.Model):
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+
+
+class PostTag(db.Model):
+
+    __tablename__ = 'post_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
